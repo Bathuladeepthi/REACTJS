4ab94a09e009f8403f701731d62b076787ee8b5c
gyapika <bathuladeepthi7367@gmail.com>
2023-04-16 17:32:36 +0530

    all intial files

---
+++ gyapikaapp/src/gnyapikacourses.js
@@ -0,0 +1,17 @@
+import react from 'react';
+class gnyapikacourses extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={username:''};
+    }
+    render(){
+        return(
+            <div>
+                this is gnyapikacourses classes
+            </div>
+        )
+    }
+    
+    
+
+}
